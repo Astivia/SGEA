7772e36ed5ee13e20c7d12e5aff1fd5d2d0fcaf7
Astivia <max.astivia13@gmail.com>
2024-06-24 11:19:55 -0600

    trabajando en Roles y Livewire

--- resources/views/Areas/index.blade.php
+++ resources/views/Areas/index.blade.php
@@ -15,45 +15,9 @@
 <br><br>
 <div class="container">
     <h1>Lista de Areas</h1>
-    <table id="example" class="table table-striped" style="width:100%">
-        <thead>
-            <tr>
-                <th>Nombre</th>
-                <th>Descripcion</th>
-                <th>Controles</th>
-            </tr>
-
-        </thead>
-        <tbody>
-            @foreach ($Areas as $area)
-            <tr>
-                <td>{!!$area->nombre!!}</td>
-                <td>{!!$area->descripcion!!}</td>
-                <td>
-                    <a href="{!!'areas/'.$area->id.'/edit'!!}">
-                        <button>editar</button>
-                    </a>
-                    <a href="{{url('areas/'.$area->id)}}"
-                        onclick="event.preventDefault(); if (confirm('¿Estás seguro de que deseas eliminar este registro?')) { document.getElementById('delete-form-{{ $area->id }}').submit(); }">
-                        <button>Eliminar</button>
-                    </a>
-                    <form id="delete-form-{{ $area->id }}" action="{{ url('areas/'.$area->id) }}" method="POST"
-                        style="display: none;">
-                        @csrf
-                        @method('DELETE')
-                    </form>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-        <tfoot>
-            <tr>
-                <th>Nombre</th>
-                <th>Descripcion</th>
-                <th>Controles</th>
-            </tr>
-        </tfoot>
-    </table>
+    @livewire ('areas-index')
+    @livewireScripts
+    
 </div>
 
 <div id="create-event-modal" class="modal">
